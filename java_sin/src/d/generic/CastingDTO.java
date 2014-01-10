fc95fcaf515c340c4d0ba861c665cb0434f2be40
kimjaemin <kimjaemin@kimjaemin-PC>
2014-01-10 16:25:37 +0900

    first commit in my git repository

---
+++ java_sin/src/d/generic/CastingDTO.java
@@ -0,0 +1,11 @@
+package d.generic;
+
+public class CastingDTO {
+	private Object object;
+	public Object getObject() {
+		return object;
+	}
+	public void setObject(Object object) {
+		this.object = object;
+	}
+}
